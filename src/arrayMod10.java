571c6a228829b49789c14ebfac9a543e882d8543
Jen Dales <dalesjr@mail.uc.edu>
2024-07-22 15:17:37 -0400

    Commit

---
+++ src/arrayMod10.java
@@ -0,0 +1,29 @@
+
+public class arrayMod10
+{
+    public static void main(String[] args)
+    {
+        //Task 1--Declare the array of type int named dataPoints with a length of 100
+        int[] dataPoints = new int[100];
+
+        // Task 2: Initialize each element in dataPoints to a random value between 1 and 100.
+        Random rnd = new Random();
+        for (int i = 0; i < dataPoints.length; i++)
+        {
+            dataPoints[i] = rnd.nextInt(100) + 1;
+        }
+
+        // Task 3: Display the dataPoints values separated by " | ".
+        System.out.print("DataPoints: ");
+        for (int i = 0; i < dataPoints.length; i++)
+        {
+            System.out.print(dataPoints[i]);
+            if (i < dataPoints.length - 1)
+            {
+                System.out.print(" | ");
+            }
+        }
+        System.out.println();
+
+    }
+}
